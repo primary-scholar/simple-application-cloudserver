868db9698c2c6796fd950f5f1c14c5cd9e3629a6
luyahui <primaryscholar@126.com>
2024-11-23 16:14:19 +0800

    project init

---
+++ src/main/java/com/application/cloudserver/api/RpcResult.java
@@ -0,0 +1,14 @@
+package com.application.cloudserver.api;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+public class RpcResult<T> implements Serializable {
+    private Integer code;
+    private String message;
+    private T data;
+}
---
+++ src/main/java/com/application/cloudserver/application/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.application.cloudserver.application;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+}

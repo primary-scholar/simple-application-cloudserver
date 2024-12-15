87cefdc25f4fbe128caae485981b6e086d621005
luyahui <primaryscholar@126.com>
2024-12-15 13:52:24 +0800

    feat: 调整端口号

--- src/main/java/com/application/cloudserver/CalculateApplication.java
+++ src/main/java/com/application/cloudserver/CalculateApplication.java
@@ -8,7 +8,7 @@ public class CalculateApplication {
 
     public static void main(String[] args) {
         SohuCloudHttpServer server = SohuCloudHttpServer.create().scanPackageList(Arrays.asList("com.application" +
-                ".cloudserver")).port(8082).supportSpring().build();
+                ".cloudserver")).port(8083).supportSpring().build();
         server.start();
     }
 }
